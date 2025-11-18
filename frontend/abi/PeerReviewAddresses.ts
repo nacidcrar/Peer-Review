8795eb9a058b7fa7cc04dddc9c884823e68280c6
LionPearNews <hausuwkwhsg@gmail.com>
2025-11-18 23:15:05 +0800

    Add addresses file

---
+++ frontend/abi/PeerReviewAddresses.ts
@@ -0,0 +1,9 @@
+
+/*
+  This file is auto-generated.
+  Command: 'npm run genabi'
+*/
+export const PeerReviewAddresses = { 
+  "31337": { address: "0x0000000000000000000000000000000000000000", chainId: 31337, chainName: "hardhat" },
+  "11155111": { address: "0xF5936a6C6aA3C0c1089063ACAD0fA6085d9aFc55", chainId: 11155111, chainName: "sepolia" }
+};
